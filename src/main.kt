8f0fe1c4af5b78fff1ef533b6c25136dd36b1c73
winniemwangi <winniewameremwangi@gmail.com>
2022-02-28 18:04:51 +0300

    add file

---
+++ src/main.kt
@@ -0,0 +1,34 @@
+fun main(){
+    var name = "AkiraChix"
+    println(name[1].toString()+name[3]+name[4])
+
+
+    var statement = "hi, my name is"
+    var chars = "winnie"
+    var conclusion = "and I'm 20 years old"
+
+    println(statement+" "+chars+" "+conclusion)
+
+    var country = "kenya"
+    println(country.length)
+
+    var first = "winnie"
+    var second = "winnie"
+    if (first==second){
+        println("That's me")
+    }
+    else{(first==second)
+        println("I don't know who it is")
+
+    }
+
+    var a = 8
+    var b = 7
+
+    if(a<b){
+        println(true)
+    }else {
+        println(false)
+    }
+
+}
